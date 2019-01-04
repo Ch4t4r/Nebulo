c02510f9bed4c4cc6a9ea207a15cf04cee6eeb7d
Daniel Wolf <daniel.wolf@frostnerd.com>
2019-01-04 14:16:42 +0100

    The app can now open links for uploaded .dohserver formats directly

--- app/src/main/java/com/frostnerd/smokescreen/activity/ServerImportActivity.kt
+++ app/src/main/java/com/frostnerd/smokescreen/activity/ServerImportActivity.kt
@@ -2,7 +2,6 @@ package com.frostnerd.smokescreen.activity
 
 import android.net.Uri
 import android.os.Bundle
-import android.view.Window
 import androidx.appcompat.app.AlertDialog
 import com.frostnerd.encrypteddnstunnelproxy.HttpsDnsServerInformation
 import com.frostnerd.encrypteddnstunnelproxy.HttpsDnsServerInformationTypeAdapter
@@ -13,7 +12,11 @@ import com.frostnerd.smokescreen.dialog.ServerImportDialog
 import com.frostnerd.smokescreen.getPreferences
 import com.google.gson.stream.JsonReader
 import com.google.gson.stream.JsonToken
+import kotlinx.coroutines.*
+import java.io.InputStream
 import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 /**
  * Copyright Daniel Wolf 2018
@@ -28,54 +31,82 @@ class ServerImportActivity : BaseActivity() {
     override fun getConfiguration(): Configuration {
         return Configuration.withDefaults()
     }
+    private var importJob: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(getPreferences().theme.dialogStyle)
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.dialogactivity_server_import)
         supportActionBar?.hide()
         actionBar?.hide()
 
-        var couldImport = false
-        var exception:java.lang.Exception? = null
         if (intent != null) {
             val data = intent.data
             if (data != null) {
-                couldImport = try {
-                    importServerFromUri(data)
-                } catch (e: Exception) {
-                    exception = e
-                    e.printStackTrace()
-                    false
+                importJob =  GlobalScope.launch {
+                    var exception: java.lang.Exception? = null
+                    val couldImport: Boolean = try {
+                        importServerFromUri(data)
+                    } catch (e: Exception) {
+                        exception = e
+                        e.printStackTrace()
+                        false
+                    }
+
+                    if (!couldImport) {
+                        launch(Dispatchers.Main) {
+                            AlertDialog.Builder(this@ServerImportActivity, getPreferences().theme.dialogStyle)
+                                .setTitle(R.string.dialog_serverimportfailed_title)
+                                .setMessage(
+                                    if (exception != null) getString(
+                                        R.string.dialog_serverimportfailed_text_exception,
+                                        Logger.stacktraceToString(exception)
+                                    ) else getString(R.string.dialog_serverimportfailed_text)
+                                )
+                                .setNeutralButton(R.string.ok) { dialog, _ ->
+                                    dialog.cancel()
+                                }
+                                .setOnCancelListener {
+                                    finish()
+                                }
+                                .setOnDismissListener {
+                                    finish()
+                                }.show()
+                        }
+                    }
+                    importJob = null
                 }
             }
         }
-        if (!couldImport) {
-            AlertDialog.Builder(this, getPreferences().theme.dialogStyle)
-                .setTitle(R.string.dialog_serverimportfailed_title)
-                .setMessage(if(exception != null) getString(R.string.dialog_serverimportfailed_text_exception, Logger.stacktraceToString(exception)) else getString(R.string.dialog_serverimportfailed_text))
-                .setNeutralButton(R.string.ok) { dialog, _ ->
-                    dialog.cancel()
-                }
-                .setOnCancelListener {
-                    finish()
-                }
-                .setOnDismissListener {
-                    finish()
-                }.show()
-        }
     }
 
-    private fun importServerFromUri(uri: Uri): Boolean {
-        val resolver = contentResolver
-        val inputStream = resolver.openInputStream(uri)
-        if (inputStream != null) {
-            val reader = JsonReader(InputStreamReader(inputStream))
-            val rtrn = readJson(reader)
-            reader.close()
-            return rtrn
-        }
-        return false
+    override fun onDestroy() {
+        super.onDestroy()
+        importJob?.cancel()
+    }
+
+    private suspend fun importServerFromUri(uri: Uri): Boolean {
+        return GlobalScope.async {
+            val inputStream: InputStream?
+            if (uri.scheme == "content" || uri.schemeSpecificPart == "file") {
+                val resolver = contentResolver
+                inputStream = resolver.openInputStream(uri)
+
+            } else if (uri.scheme == "https" || uri.scheme == "http") {
+                val url = URL(uri.toString())
+                val connection = url.openConnection() as HttpURLConnection
+                connection.instanceFollowRedirects = true
+                connection.readTimeout = 5000
+                connection.connectTimeout = 5000
+                inputStream = connection.inputStream
+            } else inputStream = null
+            if(inputStream != null) {
+                val reader = JsonReader(InputStreamReader(inputStream))
+                val rtrn = readJson(reader)
+                reader.close()
+                rtrn
+            } else false
+        }.await()
     }
 
     private fun readJson(reader: JsonReader): Boolean {
@@ -95,11 +126,13 @@ class ServerImportActivity : BaseActivity() {
 
         return if (servers.isEmpty()) false
         else {
-            val dialog = ServerImportDialog(this, servers)
-            dialog.setOnDismissListener {
-                finish()
+            GlobalScope.launch(Dispatchers.Main) {
+                val dialog = ServerImportDialog(this@ServerImportActivity, servers)
+                dialog.setOnDismissListener {
+                    finish()
+                }
+                dialog.show()
             }
-            dialog.show()
             true
         }
     }
