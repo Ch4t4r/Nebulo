1c76406ff18a4f2a0e81e02da8773bdf9972b008
Daniel Wolf <daniel.wolf@frostnerd.com>
2019-06-29 01:14:36 +0200

    Deleting the rules async throws an exception when the source is delete before the rules referencing it

--- app/src/main/java/com/frostnerd/smokescreen/activity/DnsRuleActivity.kt
+++ app/src/main/java/com/frostnerd/smokescreen/activity/DnsRuleActivity.kt
@@ -33,24 +33,26 @@ import kotlinx.android.synthetic.main.item_datasource.view.enable
 import kotlinx.android.synthetic.main.item_datasource.view.text
 import kotlinx.android.synthetic.main.item_datasource_rules.view.*
 import kotlinx.android.synthetic.main.item_dnsrule_host.view.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlin.system.measureTimeMillis
 
 /*
  * Copyright (C) 2019 Daniel Wolf (Ch4t4r)
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- 
+
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- 
+
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * You can contact the developer at daniel.wolf@frostnerd.com.
  */
 class DnsRuleActivity : BaseActivity() {
@@ -114,8 +116,10 @@ class DnsRuleActivity : BaseActivity() {
                             val pos = sourceAdapterList.indexOf(it)
                             sourceAdapterList.removeAt(pos)
                             sourceAdapter.notifyItemRemoved(pos)
-                            getDatabase().dnsRuleRepository().deleteAllFromSourceAsync(it)
-                            getDatabase().hostSourceRepository().deleteAsync(it)
+                            GlobalScope.launch {
+                                getDatabase().dnsRuleDao().deleteAllFromSource(it.id)
+                                getDatabase().hostSourceDao().delete(it)
+                            }
                             dialog.dismiss()
                         }, getString(R.string.all_no) to { dialog, _ ->
                             dialog.dismiss()
