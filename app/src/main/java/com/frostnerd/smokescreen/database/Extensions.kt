9c6bd960cb74f7000ecb938a4cacb90260e8e3b2
Daniel Wolf <daniel.wolf@frostnerd.com>
2019-07-13 21:29:47 +0200

    Create the index if it hasn't existed

--- app/src/main/java/com/frostnerd/smokescreen/database/Extensions.kt
+++ app/src/main/java/com/frostnerd/smokescreen/database/Extensions.kt
@@ -63,7 +63,7 @@ private val MIGRATION_6_7 = migration(6,7) {
     it.execSQL("DROP INDEX IF EXISTS `index_DnsRule_host`")
     it.execSQL("DROP INDEX IF EXISTS `index_DnsRule_host_type`")
     it.execSQL("DELETE FROM `DnsRule`")
-    it.execSQL("CREATE UNIQUE INDEX `index_DnsRule_host_type_stagingType` ON `DnsRule` (`host`, `type`, `stagingType`)")
+    it.execSQL("CREATE UNIQUE INDEX IF NOT EXISTS `index_DnsRule_host_type_stagingType` ON `DnsRule` (`host`, `type`, `stagingType`)")
     Logger.logIfOpen("DB_MIGRATION", "Migration from 6 to 7 completed")
 }
 
