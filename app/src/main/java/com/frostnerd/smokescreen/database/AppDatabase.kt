3fff34e53e23f6842bb29feab894303f0d49455c
Daniel Wolf <daniel.wolf@frostnerd.com>
2019-07-14 21:23:35 +0200

    stagingType has to be non-null for the index to actually do anything (https://www.sqlite.org/lang_createindex.html -- NULL values are always considered unique)

--- app/src/main/java/com/frostnerd/smokescreen/database/AppDatabase.kt
+++ app/src/main/java/com/frostnerd/smokescreen/database/AppDatabase.kt
@@ -37,7 +37,7 @@ import com.frostnerd.smokescreen.database.repository.HostSourceRepository
 @Database(entities = [CachedResponse::class, DnsQuery::class, DnsRule::class, HostSource::class], version = AppDatabase.currentVersion)
 abstract class AppDatabase : RoomDatabase() {
     companion object {
-        const val currentVersion:Int = 7
+        const val currentVersion:Int = 8
     }
 
     abstract fun cachedResponseDao(): CachedResponseDao
