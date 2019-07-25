89175556eece0e21ad7def9afd6c37c44a47d6c5
Daniel Wolf <daniel.wolf@frostnerd.com>
2019-07-26 01:50:40 +0200

    Added a dialog to inform about battery optimizations (no content yet)

---
+++ app/src/main/java/com/frostnerd/smokescreen/dialog/BatteryOptimizationInfoDialog.kt
@@ -0,0 +1,25 @@
+package com.frostnerd.smokescreen.dialog
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+import com.frostnerd.smokescreen.getPreferences
+
+/*
+ * Copyright (C) 2019 Daniel Wolf (Ch4t4r)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact the developer at daniel.wolf@frostnerd.com.
+ */
+class BatteryOptimizationInfoDialog(context:Context) :AlertDialog(context, context.getPreferences().theme.dialogStyle)
