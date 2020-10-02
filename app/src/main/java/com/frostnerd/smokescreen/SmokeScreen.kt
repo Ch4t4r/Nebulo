18fdb945243b26f2083e52f5cc7dd32fb35dafed
Daniel Wolf <daniel.wolf@frostnerd.com>
2020-10-02 22:53:42 +0200

    Use languagewrapper in application class

--- app/src/main/java/com/frostnerd/smokescreen/SmokeScreen.kt
+++ app/src/main/java/com/frostnerd/smokescreen/SmokeScreen.kt
@@ -14,6 +14,7 @@ import com.frostnerd.smokescreen.activity.ErrorDialogActivity
 import com.frostnerd.smokescreen.activity.LoggingDialogActivity
 import com.frostnerd.smokescreen.activity.PinActivity
 import com.frostnerd.smokescreen.database.AppDatabase
+import com.frostnerd.smokescreen.util.LanguageContextWrapper
 import com.frostnerd.smokescreen.util.Notifications
 import com.frostnerd.smokescreen.util.RequestCodes
 import com.frostnerd.smokescreen.util.crashhelpers.DataSavingSentryEventProcessor
@@ -134,6 +135,10 @@ class SmokeScreen : Application() {
             Notifications.ID_APP_CRASH,
             notification.build()
         )
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(LanguageContextWrapper.attachFromSettings(this, newBase))
     }
 
     override fun onCreate() {
